0e663b3dee7e9f9628f2f617db59f750bafecdf6
mkhs <mkh_sharif@aol.com>
2019-01-01 17:43:48 -0500

    dropdown closes when selection made

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
+
+var dropdown =  document.getElementById('menu');
+
+
+document.getElementById('selector').addEventListener('mouseover', openDropDown);
+document.getElementById('selector').addEventListener('mouseout', closeDropDown);
 document.getElementById('select1').addEventListener('click', updateDropDown);
 document.getElementById('select2').addEventListener('click', updateDropDown);
 
 
+function openDropDown() {
+    if(dropdown.classList.contains('closed')) {
+        dropdown.classList.remove('closed');
+        dropdown.classList.add('open');
+    }
+}
+
+function closeDropDown() {
+    if(dropdown.classList.contains('open')) {
+        dropdown.classList.remove('open');
+        dropdown.classList.add('closed');
+    }
+}
+
 
 function updateDropDown() {
     var top = document.getElementById('top').textContent;
-    var selected = event.target.textContent;
+    var selected = this.textContent;
     document.getElementById('top').textContent = selected;
-    event.target.textContent = top;
-    
-   
+    this.textContent = top;   
+    closeDropDown();
 }
